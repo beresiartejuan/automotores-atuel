b72e464f7f930e1c4ed5beaa4b08723b834ce957
juan beresiarte <juanberesiarte@gmail.com>
2024-12-03 20:08:43 -0300

    Create Head component and use vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "http://localhost:4321",
+    vite: {
+        css: {
+            preprocessorOptions: {
+                sass: {
+                    api: "modern-compiler",
+                },
+                scss: {
+                    api: "modern-compiler",
+                },
+            },
+        },
+    },
+    output: "server",
+    adapter: vercel()
+});
